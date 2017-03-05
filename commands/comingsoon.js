bf51c8c8beffc89e6242e1afb30afb12536b50df
Samazer2 <sammymckay38@hotmail.com>
2017-03-05 07:56:57 +0000

    general edits for me

--- commands/comingsoon.js
+++ commands/comingsoon.js
@@ -6,33 +6,27 @@ module.exports = {
     msg.channel.sendEmbed(new Discord.RichEmbed()
       .setTitle('Coming soon to Rokkit')
       .setColor(msg.guild.member(client.user).highestRole.color)
-      .setDescription(`1. Customisable Bio in >> profile
+      .setDescription(`Customisable Bio in >> profile
 
-        2. Changeable prefix for different servers
+        Changeable prefix for different servers
 
-        3. Reminder managment
+        Private room maker
 
-        4. Role commands (manipulate roles, give, take, add, remove, new user roles, ect)
+        Bugs found for Rokkit Bug Testers
 
-        5. Private room maker
+        Add google/youtube/image/emoji/ect searching
 
-        6. Bugs found for Rokkit Bug Testers
+        Weather updates
 
-        7. Add google/youtube/image/emoji/ect searching
+        make this command automated so i can add and remove todos in chat, or make global todos for everyone
 
-        8. Message purging
+        define command
 
-        9. Weather updates
+        translate command
 
-        10. make this command automated so i can add and remove todos in chat, or make global todos for everyone
+        be able to use >> avatar @mention
 
-        11. define command
-
-        12. translate command
-
-        13. be able to use >> avatar @mention
-
-        14. fix reminder no time / subject`)
+        fix reminder no time / subject`)
       .setThumbnail(`${client.users.get('284894725998379019').avatarURL.replace('.jpg', '.png')}`))
   },
   args: 'There are no arguments for this command!',
