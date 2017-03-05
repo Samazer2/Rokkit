a58dbb22f93571278050875577b35ae4a74c1fce
Samazer2 <sammymckay38@hotmail.com>
2017-03-05 07:55:34 +0000

    Can now @mention users

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,9 +1,16 @@
 const config = require('../config.json');
 module.exports = {
 
-  func: (client, msg, args, config) => {
-    let attachment = msg.author.displayAvatarURL.replace('.jpg', '.png')
-    msg.channel.sendFile(attachment, 'avatar.png', `Here is your Avatar ${msg.author}`)
+  func: (client, msg, args) => {
+    if (msg.mentions.users.size > 0) {
+      var attachment = msg.guild.member(msg.mentions.users.first()).user.displayAvatarURL.replace('.jpg', '.png')
+      var reply = `Here is ${msg.guild.member(msg.mentions.users.first()).user.username}\'s Avatar`
+    }
+    else {
+      var attachment = msg.author.displayAvatarURL.replace('.jpg', '.png')
+      var reply = `Here is your Avatar ${msg.author}`
+    }
+    msg.channel.sendFile(attachment, 'avatar.png', reply)
   },
   args: 'There are no arguments for this command!',
   help: `This command will upload a .png of your avatar, use: ${config.prefix}avatar`,
