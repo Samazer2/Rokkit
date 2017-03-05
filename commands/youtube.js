23c90771198027c26e6993582af8ebdc3a7972ad
Samazer2 <sammymckay38@hotmail.com>
2017-03-05 19:27:59 +0000

    new youtube search command

---
+++ commands/youtube.js
@@ -0,0 +1,23 @@
+const search = require('youtube-search');
+const apikey = require('../config.json').apikey
+
+module.exports = {
+
+  func: (client, msg, args) => {
+    if (args.length < 1) return msg.channel.sendMessage(`:warning: **${msg.author.username}, you must provide arguments**`);
+    msgcontent = args[0].toString()
+
+    var opts = {
+      maxResults: 1,
+      key: apikey
+    };
+
+    search(msgcontent, opts, function(err, results) {
+      if(err) return console.log(err);
+
+    msg.channel.sendMessage(`:tv: **I found** ***\'${results[0].title}\'*** **| ${results[0].link} **`)
+    });
+  },
+  args: '<what you want to search for>',
+  help: 'Search youtube for a video by providing a term',
+}
