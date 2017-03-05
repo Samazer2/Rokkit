9d1d294bc556ff85a0871f333b49a5d798d48f81
Samazer2 <sammymckay38@hotmail.com>
2017-03-05 23:39:52 +0000

    change to bot invite

--- commands/invite.js
+++ commands/invite.js
@@ -2,8 +2,8 @@ const config = require('../config.json');
 module.exports = {
 
   func: (client, msg, args) => {
-    msg.channel.sendMessage('Use this link to join the server,  https://discord.gg/DBZx7XS')
+    msg.channel.sendMessage('Use this link to get me on your server, https://discordapp.com/oauth2/authorize?client_id=284894725998379019&scope=bot&permissions=2146958463')
   },
   args: 'There are no arguments for this command!',
-  help: `This command will post an invite link for the ssd server, use: ${config.prefix}invite`,
+  help: `This command will post an invite link for rokkit, use: ${config.prefix}invite`,
 }
